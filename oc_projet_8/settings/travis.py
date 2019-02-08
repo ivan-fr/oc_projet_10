f9659bb4d3e081b5fbfe5dea18fc7150d21e26c1
besevic <ivan.besevic_fr@yahoo.com>
2019-02-08 09:55:56 +0100

    3ème push

---
+++ oc_projet_8/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "oc_projet_8",
+        "USER": "besevic",
+        "PASSWORD": "hWfY7Uv82k7L9f2Sr._.",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
