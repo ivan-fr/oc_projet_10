4e1c69916f28170331808d312ffeb89dfc3a6679
besevic <ivan.besevic_fr@yahoo.com>
2019-02-09 11:06:48 +0100

    push

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn oc_projet_8.wsgi
--- oc_projet_8/settings/travis.py
+++ oc_projet_8/settings/travis.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": "travis_ci_test",
         "USER": "besevic",
-        "PASSWORD": "hWfY7Uv82k7L9f2Sr._.",
+        "PASSWORD": "unpassword",
         "HOST": "localhost",
         "PORT": "5432",
     }
